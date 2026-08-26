6741d9f650e1f62bfdb4eb3faa8adcd5929a8e15
Jenay Han <jenayh305@gmail.com>
2026-08-26 14:32:53 -0700

    Day 1 Initial Setup

---
+++ Setup.java
@@ -0,0 +1,6 @@
+public class Setup{
+    public static void main(String[] args) {
+        System.out.print("the duck waddled to the lemonade stand and he said to the man running the stand, hey bump bump bump, do you have any grapes?");
+    }
+}
+
