d4beb482117fb90a36158564544ecf1d4dae6265
Jenay Han <jenayh305@gmail.com>
2026-09-01 10:34:21 -0700

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,5 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.print("Name: Jenay Han\nBirthday: January 6, 2011\nHobbies: Squash (the sport), reading, chess, the violin\nFavorite Book: \"What If?\"\nFavorite Movie: Interstellar");
+            }
+}
---
+++ BlockLetters.java
@@ -0,0 +1,6 @@
+public class BlockLetters {
+    public static void main(String[] args) {
+        System.out.print("JJJJ \n  J \n  J \n  J \nJJJ \n");
+        System.out.println("\nH H\nH H\nHHH\nH H\nH H");
+    }
+}
